0ab42ec33b45b7ca5c1c7901af47a069d8571656
mounsif.meftah <mounsif.meftah@2wls.net>
2023-05-23 20:34:01 +0100

    Add search screen header

--- src/screens/logged/search/index.js
+++ src/screens/logged/search/index.js
@@ -1,18 +1,42 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import colors from '../../../common/colors';
+import { IconButton, Text } from 'react-native-paper';
 
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
 
 	return (
-		<View>
-			<Text>Search screen</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<View style={styles.row}>
+				<Text variant="titleLarge" style={styles.title}>RECHERCHE</Text>
+				<IconButton
+					icon="close"
+					onPress={navigation.goBack}
+				/>
+			</View>
+		</SafeAreaView>
 	)
 }
 
 const styles = StyleSheet.create({
-
+	container: {
+		width: "100%",
+		flex: 1,
+		paddingHorizontal: 18,
+		backgroundColor: colors.white
+	},
+	row: {
+		width: "100%",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		paddingVertical: 10
+	},
+	title: {
+		fontWeight: "900"
+	}
 });
 
 export default SearchScreen
