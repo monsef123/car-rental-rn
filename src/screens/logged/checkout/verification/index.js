a0a635b24c0c841f4786d962c8edfc901fdd96e3
mounsif.meftah <mounsif.meftah@2wls.net>
2023-05-25 14:09:38 +0100

    Finalize checkout verification screen UI

--- src/screens/logged/checkout/verification/index.js
+++ src/screens/logged/checkout/verification/index.js
@@ -7,94 +7,109 @@ import { Text } from 'react-native-paper';
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import CustomChip from '../../../../components/CustomChip';
 import CheckBox from '../../../../components/CheckBox';
+import BigIconButton from '../../../../components/BigIconButton';
 
 
 const CheckoutVerification = ({ navigation }) => {
 	const [paymentMethod, setPaymentMethod] = useState();
 
+	const onSubmitVerification = () => {
+		// TODO: Add logic here
+	}
+
 	return (
-		<ScrollView
-			contentContainerStyle={styles.scrollView}
-			bounces={false}
-			showsVerticalScrollIndicator={false}
-		>
-			<SafeAreaView style={styles.container}>
-				<HeaderWithBack
-					goBack={navigation.goBack}
-					title="VÉRIFIEZ VOS INFORMATIONS"
-				/>
-			</SafeAreaView>
-			<View style={{ paddingHorizontal: 18 }}>
-				<View style={styles.containerCard}>
-					<Text style={styles.cardLabelText}>Renseigner votre address de livraison</Text>
-					<TextInput
-						style={styles.input}
-						placeholder='Adresse'
-						placeholderTextColor="#666"
+		<>
+			<ScrollView
+				contentContainerStyle={styles.scrollView}
+				bounces={false}
+				showsVerticalScrollIndicator={false}
+			>
+				<SafeAreaView style={styles.container}>
+					<HeaderWithBack
+						goBack={navigation.goBack}
+						title="VÉRIFIEZ VOS INFORMATIONS"
 					/>
-				</View>
-				<View style={[styles.containerCard, { marginTop: 24 }]}>
-					<Text style={styles.cardLabelText}>Renseigner l’address de retour du véhicule</Text>
-					<TextInput
-						style={styles.input}
-						placeholder='Adresse'
-						placeholderTextColor="#666"
-					/>
-				</View>
-				<View style={[styles.containerCard, { marginTop: 24 }]}>
-					<View style={styles.row}>
-						<Icon name="checkbox-marked-circle" size={26} />
-						<Text style={[styles.cardLabelText, { fontWeight: "400", paddingLeft: 10 }]}>Numéro de téléphone</Text>
-					</View>
-					<TextInput
-						style={styles.input}
-						placeholder='0000 000 0000'
-						placeholderTextColor="#666"
-						keyboardType='phone-pad'
-						returnKeyType='done'
-					/>
-				</View>
-				<View style={[styles.containerCard, { marginTop: 24 }]}>
-					<View style={styles.row}>
-						<Icon name="checkbox-marked-circle" size={26} color={colors.lighGray} />
-						<Text style={[styles.cardLabelText, { fontWeight: "400", paddingLeft: 10 }]}>Permis de conduire</Text>
-					</View>
-					<View style={[styles.row, { marginVertical: 16 }]}>
-						<CustomChip
-							label="Choisir le fichier"
-							textStyle={{ color: colors.white, fontSize: 16, fontWeight: "400" }}
-							style={{ backgroundColor: colors.textBlack, paddingHorizontal: 0 }}
-						/>
-						<Text style={{ paddingLeft: 16 }}>Pas de fichier choisi</Text>
-					</View>
-					<View style={styles.row}>
-						<CustomChip
-							label="Prendre une photo"
-							textStyle={{ color: colors.white, fontSize: 16, fontWeight: "400" }}
-							style={{ backgroundColor: colors.textBlack, paddingHorizontal: 0 }}
+				</SafeAreaView>
+				<View style={{ paddingHorizontal: 18 }}>
+					<View style={styles.containerCard}>
+						<Text style={styles.cardLabelText}>Renseigner votre address de livraison</Text>
+						<TextInput
+							style={styles.input}
+							placeholder='Adresse'
+							placeholderTextColor="#666"
 						/>
 					</View>
+					<View style={[styles.containerCard, { marginTop: 24 }]}>
+						<Text style={styles.cardLabelText}>Renseigner l’address de retour du véhicule</Text>
+						<TextInput
+							style={styles.input}
+							placeholder='Adresse'
+							placeholderTextColor="#666"
+						/>
+					</View>
+					<View style={[styles.containerCard, { marginTop: 24 }]}>
+						<View style={styles.row}>
+							<Icon name="checkbox-marked-circle" size={26} />
+							<Text style={[styles.cardLabelText, { fontWeight: "400", paddingLeft: 10 }]}>Numéro de téléphone</Text>
+						</View>
+						<TextInput
+							style={styles.input}
+							placeholder='0000 000 0000'
+							placeholderTextColor="#666"
+							keyboardType='phone-pad'
+							returnKeyType='done'
+						/>
+					</View>
+					<View style={[styles.containerCard, { marginTop: 24 }]}>
+						<View style={styles.row}>
+							<Icon name="checkbox-marked-circle" size={26} color={colors.lighGray} />
+							<Text style={[styles.cardLabelText, { fontWeight: "400", paddingLeft: 10 }]}>Permis de conduire</Text>
+						</View>
+						<View style={[styles.row, { marginVertical: 16 }]}>
+							<CustomChip
+								label="Choisir le fichier"
+								textStyle={{ color: colors.white, fontSize: 16, fontWeight: "400" }}
+								style={{ backgroundColor: colors.textBlack, paddingHorizontal: 0 }}
+							/>
+							<Text style={{ paddingLeft: 16 }}>Pas de fichier choisi</Text>
+						</View>
+						<View style={styles.row}>
+							<CustomChip
+								label="Prendre une photo"
+								textStyle={{ color: colors.white, fontSize: 16, fontWeight: "400" }}
+								style={{ backgroundColor: colors.textBlack, paddingHorizontal: 0 }}
+							/>
+						</View>
+					</View>
+					<View style={[styles.containerCard, { marginTop: 24 }]}>
+						<Text style={styles.cardLabelText}>Mode de paiement</Text>
+						<CheckBox
+							label="Paiement par carte de crédit"
+							isChecked={paymentMethod === 0}
+							onPress={() => setPaymentMethod(0)}
+						/>
+						<CheckBox
+							label="Paiement par chèque"
+							isChecked={paymentMethod === 1}
+							onPress={() => setPaymentMethod(1)}
+						/>
+						<CheckBox
+							label="Paiement en espèces"
+							isChecked={paymentMethod === 2}
+							onPress={() => setPaymentMethod(2)}
+						/>
+					</View>
 				</View>
-				<View style={[styles.containerCard, { marginTop: 24 }]}>
-					<Text style={styles.cardLabelText}>Mode de paiement</Text>
-					<CheckBox
-						label="Paiement par carte de crédit"
-						isChecked={paymentMethod === 0}
-						onPress={() => setPaymentMethod(0)}
-					/>
-					<CheckBox
-						label="Paiement par chèque"
-						isChecked={paymentMethod === 1}
-						onPress={() => setPaymentMethod(1)}
-					/>
-					<CheckBox
-						label="Paiement en espèces"
-						isChecked={paymentMethod === 2}
-						onPress={() => setPaymentMethod(2)}
-					/>
-				</View>
-			</View>
-		</ScrollView>
+			</ScrollView>
+			{/** Validate button */}
+			<BigIconButton
+				iconName="arrow-right"
+				style={styles.nextBtn}
+				iconColor={colors.white}
+				containerColor={colors.textBlack}
+				onPress={onSubmitVerification}
+			/>
+		</>
 	)
 }
 
@@ -133,6 +148,11 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		alignItems: "center",
 		justifyContent: "flex-start"
+	},
+	nextBtn: {
+		position: "absolute",
+		bottom: 60,
+		right: 20
 	}
 });
 
